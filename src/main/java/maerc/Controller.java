b1ca1408360d81bff19c0f19c244137f34b5062c
Raúl Fernández Álvarez <funny.it.worked.last.time@runbox.com>
2017-12-29 13:46:10 +0100

    Extract User Input from UI

--- src/main/java/maerc/Controller.java
+++ src/main/java/maerc/Controller.java
@@ -1,20 +1,46 @@
 package maerc;
 
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Controller {
+
+    @FXML
+    private VBox illnessesPane;
+
+    @FXML
+    private VBox previousInfoPane;
+
+    @FXML
+    private VBox symptonsPane;
 
     @FXML
     private VBox resultsPane;
 
+    private Map<String, Boolean> getInputFromPane (VBox inputPane) {
+        FilteredList<Node> userInput = inputPane.getChildren().filtered(node -> node instanceof CheckBox);
+        return userInput.stream().map(node -> (CheckBox)node).collect(Collectors.toMap(CheckBox::getId, CheckBox::isSelected));
+    }
+
     @FXML
     protected void handleAnalysis(ActionEvent actionEvent) {
+        System.out.println(getInputFromPane(illnessesPane));
+        System.out.println(getInputFromPane(previousInfoPane));
+        System.out.println(getInputFromPane(symptonsPane));
+
+        // Dummy result
         final Label illnessName = new Label();
         illnessName.setText("Illness" + ":");
 
